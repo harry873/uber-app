8fd2266807b9b69de8cdf59e51ef9dcd5ff550f0
harry873 <92635139+harry873@users.noreply.github.com>
2023-05-25 12:57:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -23,3 +23,16 @@ data_load_state.text('Loading data...done!')
 
 st.subheader('Raw data')
 st.write(data)
+
+
+df = pd.DataFrame(
+    [
+       {"command": "st.selectbox", "rating": 4, "is_widget": True},
+       {"command": "st.balloons", "rating": 5, "is_widget": False},
+       {"command": "st.time_input", "rating": 3, "is_widget": True},
+   ]
+)
+edited_df = st.experimental_data_editor(df)
+
+favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
